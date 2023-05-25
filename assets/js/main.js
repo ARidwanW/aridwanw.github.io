5f12512d4f80c4b41a9b36139509516793803da6
Alwandia Ridwan Wadiska <34335705+ARidwanW@users.noreply.github.com>
2023-05-25 12:19:31 +0700

    add project file - working

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height,
+    // add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                    : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// ? apa perbedaannya dengan di bawah ini 
+// * jika di atas tidak bisa maka coba yg bawa
+// function scrollHeader(){
+//     const header = document.getElementById('header')
+//     // When the scroll is greater than 50 viewport height,
+//     // add the scroll-header class to the header tag
+//     if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+//     else header.classList.remove('scroll-header')
+// }
+// window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', () =>{
+        modalViews.forEach((mv) =>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
+// TODO: sampai di sini dulu
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/* Link active work */ 
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
